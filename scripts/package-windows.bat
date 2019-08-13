12d5d739a278fd7a6833856408a6b0afb95c7c7e
NGnius <ngniusness@gmail.com>
2019-08-13 19:42:12 -0400

    Fix batch file

--- scripts/package-windows.bat
+++ scripts/package-windows.bat
@@ -1,1 +1,2 @@
-go mod download && go get -u -v github.com/therecipe/qt/cmd/qtdeploy && go get -u -v github.com/therecipe/qt/cmd/... && go mod vendor && git clone https://github.com/therecipe/env_windows_amd64_513.git vendor/github.com/therecipe/env_windows_amd64_513 && for /f %v in ('go env GOPATH') do %v\bin\qtdeploy build desktop
+ECHO OFF
+go mod download && go get -u -v github.com/therecipe/qt/cmd/qtdeploy && go get -u -v github.com/therecipe/qt/cmd/... && go mod vendor && git clone https://github.com/therecipe/env_windows_amd64_513.git vendor/github.com/therecipe/env_windows_amd64_513 && for /f %%v in ('go env GOPATH') do %%v\bin\qtdeploy build desktop
