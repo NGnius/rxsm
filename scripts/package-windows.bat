5ba0f757bfe149a7bc0072d40b4bed78c0edef20
NGnius <ngniusness@gmail.com>
2019-08-18 20:21:12 -0400

    Copy default_save into windows deploy folder

--- scripts/package-windows.bat
+++ scripts/package-windows.bat
@@ -1,2 +1,3 @@
 ECHO OFF
 cd .\rxsm && go mod download && go get -u -v github.com/therecipe/qt/cmd/qtdeploy && go get -u -v github.com/therecipe/qt/cmd/... && go mod vendor && git clone https://github.com/therecipe/env_windows_amd64_513.git vendor/github.com/therecipe/env_windows_amd64_513 && for /f %%v in ('go env GOPATH') do %%v\bin\qtdeploy build desktop && cd ..
+xcopy .\resources\default_save .\rxsm\deploy\windows\default_save /I /Y
